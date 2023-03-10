6670aa3d721f0c9e2d7dfd65f5c39f825b2666b1
Igor Castilhos <igorcastilhos2010@hotmail.com>
2023-03-10 12:20:43 -0300

    Adding Aula 1

---
+++ dist/index.js
@@ -0,0 +1,28 @@
+"use strict";
+function sendSpaceship(name, captain) {
+    var spaceship = {
+        name: name,
+        captain: captain,
+        speed: 20,
+        inMission: true,
+        crew: [],
+    };
+    alert("A nave ".concat(spaceship.name, " comandada pelo capit\u00E3o ").concat(spaceship.captain, " foi enviada em uma miss\u00E3o."));
+    return spaceship;
+}
+function accelerate(targetSpeed, spaceship) {
+    if (spaceship.speed > targetSpeed) {
+        alert("Reduzindo a velocidade da ".concat(spaceship.name, " para ").concat(targetSpeed, "..."));
+    }
+    else if (spaceship.speed < targetSpeed) {
+        alert("Aumentando a velocidade da ".concat(spaceship.name, " para ").concat(targetSpeed, "..."));
+    }
+    else {
+        alert("Mantendo a velocidade da ".concat(spaceship.name, "..."));
+    }
+}
+var spaceshipName = String(prompt("Insira o nome da nave a ser enviada:"));
+var spaceshipCaptain = String(prompt("Insira o nome do capitão da nave:"));
+var currentShip = sendSpaceship(spaceshipName, spaceshipCaptain);
+var speed = Number(prompt("Insira a velocidade para qual deseja acelerar: "));
+accelerate(speed, currentShip);
